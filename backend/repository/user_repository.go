f33b4e22f3249156c8d70aba9a8c04807ccf15db
ArmandoLC13 <ArmandoSchoenstatt.com>
2025-02-28 15:37:33 -0300

    feat: creating user repository

--- backend/repository/user_repository.go
+++ backend/repository/user_repository.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"database/sql"
+	"encurtador-de-link/backend/models"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) GetUserByID(id int) (*models.User, error) {
+
+	user := &models.User{}
+	err := r.db.QueryRow("Select id, name, email, password FROM users WHERE id = ?", id).Scan(
+		&user.Id, &user.Name, &user.Email, &user.Password)
+	return user, err
+
+}
